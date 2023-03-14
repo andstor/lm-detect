44ebafa39f007371ecbaaebacb3def3eece2c0eb
André Storhaug <andr3.storhaug@gmail.com>
2023-03-15 00:20:14 +0100

    Use dataset processing functions for calculating ngrams

--- scripts/trivial_ngrams.py
+++ scripts/trivial_ngrams.py
@@ -1,26 +1,43 @@
-import multiprocessing
-import os
-import time
-from multiprocessing import Pool, Queue, Process, Manager
-import random
-from datasets import load_dataset, DownloadConfig
-from tqdm import tqdm
-from transformers import AutoTokenizer
-from nltk.util import ngrams
-from collections import Counter
-import multiprocessing
-import multiprocessing.util
-import logging
-from threading import Thread
-import pickle
-import argparse
+import math
+import torch
+import torch.nn.functional as F
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM, set_seed, GenerationConfig
+from accelerate import Accelerator
+from torch.utils.data import DataLoader
+import json
 from pathlib import Path
+from tqdm import tqdm
+from typing import Optional
+from dataclasses import dataclass, field
+from transformers import (
+    CONFIG_MAPPING,
+    MODEL_MAPPING,
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    SchedulerType,
+    default_data_collator,
+    get_scheduler,
+)
+import os
+from collections import Counter
+from nltk.util import ngrams
+import pickle
+from ast import literal_eval
 
-#multiprocessing.util._logger = multiprocessing.util.log_to_stderr(logging.DEBUG)
-logging.getLogger('transformers').setLevel(logging.ERROR)
+import logging
+
+from accelerate.logging import get_logger
+get_logger("transformers").setLevel(logging.ERROR)
+logger = get_logger(__name__)
+
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Finetune a transformers model on a causal language modeling task")
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Do inference with a transformer model on a causal language modeling task")
     parser.add_argument(
         "--dataset_name",
         type=str,
@@ -34,34 +51,16 @@ def parse_args():
         help="The configuration name of the dataset to use (via the datasets library).",
     )
     parser.add_argument(
-        "--revision",
+        "--dataset_split",
         type=str,
-        default="main",
-        help="The specific revision of the dataset to use (via the datasets library).",
+        default=None,
+        help="The name of the split to use.",
     )
     parser.add_argument(
         "--text_column_name",
         type=str,
-        default="text",
-        help="The name of the text column in the dataset.",
-    )
-    parser.add_argument(
-        "--num_proc",
-        type=int,
-        default=1,
-        help="The number of processes to use for data loading.",
-    )
-    parser.add_argument(
-        "--max_samples",
-        type=int,
         default=None,
-        help="The number of sub samples to use for data loading.",
-    )
-    parser.add_argument(
-        "--output_dir",
-        type=str,
-        default="tmp",
-        help="The output directory where the trivial tokens will be saved.",
+        help="The column name of the dataset to use.",
     )
     parser.add_argument(
         "--tokenizer_name",
@@ -70,10 +69,36 @@ def parse_args():
         help="Pretrained tokenizer name or path if not the same as model_name",
     )
     parser.add_argument(
-        "--max_ngram",
+        "--use_slow_tokenizer",
+        action="store_true",
+        help="If passed, will use a slow tokenizer (not backed by the 🤗 Tokenizers library).",
+    )
+    parser.add_argument(
+        "--per_device_batch_size",  # "--per_device_train_batch_size",
         type=int,
-        default=3,
-        help="The maximum ngram to calculate.",
+        default=8,
+        help="Batch size (per device) for the dataloader.",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default=None,
+        help="Where to store the final model."
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=None,
+        help="A seed for reproducible training."
+    )
+    parser.add_argument(
+        "--preprocessing_num_workers",
+        type=int,
+        default=None,
+        help="The number of processes to use for the preprocessing.",
+    )
+    parser.add_argument(
+        "--overwrite_cache", action="store_true", help="Overwrite the cached dataset"
     )
     parser.add_argument(
         "--num_top_ngrams",
@@ -82,152 +107,153 @@ def parse_args():
         help="The k most common ngrams to return.",
     )
     parser.add_argument(
-        "--stream",
-        action="store_true",
-        help="Whether to stream the dataset.",
+        "--max_ngram",
+        type=int,
+        default=3,
+        help="The maximum ngram to calculate.",
     )
     parser.add_argument(
-        "--split_name",
-        type=str,
-        default="train",
-        help="The name of the split to use.",
+        "--num_proc",
+        type=int,
+        default=1,
+        help="The number of processes to use for data loading.",
     )
-
 
     args = parser.parse_args()
 
-    if not args.tokenizer_name:
-        raise ValueError("You need to specify a tokenizer name")
-
+    # Sanity checks
+    if args.dataset_name is None:
+        raise ValueError("Need a dataset name.")
+    
     return args
 
 
-def worker_main(queue_in, queue_out, args):
-    print(os.getpid(),"starting worker")
-    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_name)
-    while True:
-        item = queue_in.get(block=True) #block=True means make a blocking call to wait for items in queue
-        if item is None:
-            break
-
-        ngrams = calc_ngrams(item, tokenizer, args.max_ngram)
-        t_ngrams = trivial_ngrams(ngrams, args.num_top_ngrams)
-        
-        queue_out.put(t_ngrams)
-    
-    queue_out.put(None)
-
-
-def calc_ngrams(text, tokenizer, max_ngram=3):
-    tokenized = tokenizer.tokenize(text)
-    all_ngrams = []
-    for j in range(1, max_ngram+1):
-        n_grams = list(ngrams(tokenized, j))
-        all_ngrams.extend(n_grams)
-    return all_ngrams
-
-def trivial_ngrams(ngrams, k=500):
-    """Return the k most common n-grams in the corpus."""
-    # Extract trivially shared n-grams
-    frequencies = Counter(ngrams) # tokenized_corpus is a list of strings
-    return dict(frequencies.most_common(k))
-
-def merge_dicts(*dict_args):
-    """Sum all the values in the dictionaries."""
-    result = Counter()
-    for dictionary in dict_args:
-        result += Counter(dictionary)
-    return dict(result)
-
-
-
-def producer_main(que_in, args):
-    print(os.getpid(),"starting dataloader")
-    datasets = load_dataset(args.dataset_name, args.dataset_config_name, streaming=args.stream, revision=args.revision)
-    dataset = iter(datasets[args.split_name])
-
-    for i, row in enumerate(dataset):
-        if args.max_samples is not None and i >= args.max_samples:
-            break
-        que_in.put(row[args.text_column_name])
-
-    for i in range(args.num_proc):
-        que_in.put(None)
-
-
 def main():
+    """
+    Generate new data by sampling from the original data.
+    """
+
     args = parse_args()
-    print(args)
-    with Manager() as manager:
 
-        que_in = manager.Queue(maxsize=args.num_proc*2)
-        que_out = manager.Queue(maxsize=args.num_proc*2)
+    # Initialize accelerator
+    accelerator = Accelerator()
+
+    if args.seed is not None:
+        set_seed(args.seed)
+
+
+
+    # Write the generation config to disk
+    if accelerator.is_main_process:
+        if args.output_dir is not None:
+            Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+        else:
+            raise ValueError("Need a output directory.")
+    
+    accelerator.wait_for_everyone()
+
+    # 
+    # Load the dataset
+    # In distributed training, the load_dataset function guarantee that only one local process can concurrently
+    # download the dataset.
+    # if args.dataset_name is not None:
+    #    raw_datasets = load_dataset(args.dataset_name, args.dataset_config_name)
+    #    dataset = raw_datasets.with_format("torch", columns=[text_column])
+
+    if args.dataset_name is not None:
+        # Downloading and loading a dataset from the hub.
+        raw_datasets = load_dataset(
+            args.dataset_name, args.dataset_config_name)
+
+    
+    if args.tokenizer_name:
+        tokenizer = AutoTokenizer.from_pretrained(
+            args.tokenizer_name, use_fast=not args.use_slow_tokenizer)
+    else:
+        raise ValueError(
+            "You are instantiating a new tokenizer from scratch. This is not supported by this script."
+            "You can do it from another script, save it, and load it from here, using --tokenizer_name."
+        )
+    tokenizer.pad_token = tokenizer.eos_token
+
+    # Preprocessing the datasets.
+    column_names = raw_datasets[args.dataset_split].column_names
+
+    if args.text_column_name is not None:
+        text_column_name = args.text_column_name
+    else:
+        text_column_name = "text" if "text" in column_names else column_names[0]
+        logger.warning(f"Using column {text_column_name} as text column.")
+    
+
+    def top_ngrams_function(examples):
+        """Return the k most common n-grams in the corpus."""
+        k=args.num_top_ngrams
+        # Extract trivially shared n-grams
+        top_ngrams = Counter()
+        for example in examples["ngrams"]:
+            frequencies = Counter([tuple(gram) for gram in example])
+            most_common = dict(frequencies.most_common(k))
+            top_ngrams += Counter(most_common)
         
-        # Start producer
-        producer = Process(target=producer_main, args=(que_in, args))
-        producer.start()
+        return {"top_ngrams": [str(dict(top_ngrams))]}
 
-        workers = [Process(target=worker_main, args=(que_in, que_out, args)) for i in range(args.num_proc-1)]
-        for w in workers:
-            w.start()
+    def calc_ngrams_function(examples):
+        max_ngram = args.max_ngram
+        ngrams_list = []
+        for example in examples["input_ids"]:
+            all_ngrams = []
+            for j in range(1, max_ngram+1):
+                n_grams = list(ngrams(example, j))
+                all_ngrams.extend(n_grams)
+            ngrams_list.append(all_ngrams)
+        examples["ngrams"] = ngrams_list
+        return examples
+    
+    # Tokenize the data
+    def tokenize_function(examples):        
+        return tokenizer(examples[text_column_name])
+
+    with accelerator.main_process_first():
+        tokenized_datasets = raw_datasets.map(
+            tokenize_function,
+            batched=True,
+            num_proc=args.preprocessing_num_workers,
+            remove_columns=raw_datasets[args.dataset_split].column_names,
+            load_from_cache_file=not args.overwrite_cache,
+            keep_in_memory=False,
+            desc="Running tokenizer on dataset",
+        )
+        ngrams_datasets = tokenized_datasets.map(
+            calc_ngrams_function,
+            batched=True,
+            remove_columns=tokenized_datasets[args.dataset_split].column_names,
+            num_proc=args.preprocessing_num_workers,
+            load_from_cache_file=not args.overwrite_cache,
+            keep_in_memory=False,
+            desc="Calculating ngrams",
+        )
         
+        top_ngrams_datasets = ngrams_datasets.map(
+            top_ngrams_function,
+            batched=True,
+            num_proc=args.preprocessing_num_workers,
+            load_from_cache_file=not args.overwrite_cache,
+            remove_columns=ngrams_datasets[args.dataset_split].column_names,
+            keep_in_memory=False,
+            desc="Calculating top ngrams",
+        )
+
+        top_ngrams = Counter()
+        for example in top_ngrams_datasets["top_ngrams"]:
+            top_ngrams += literal_eval(example[0])
+
+        data = dict(top_ngrams.most_common(args.num_top_ngrams))
         
-        pbar = tqdm(total=args.max_samples)
-        #Collect results
-        frequencies = Counter()
-        #for i in range(args.max_samples):
-        exit_signals = 0
-        while exit_signals < args.num_proc:
-            t_ngrams = que_out.get()
-            if t_ngrams is None:
-                exit_signals += 1
-            frequencies += Counter(t_ngrams)
-            pbar.update(1)
-        pbar.close()
-        data = trivial_ngrams(frequencies)
-
-
-        # prevent adding anything more to the queue and wait for queue to empty
-        #que_in.close()
-        #que_in.join_thread()
-
-        #que_out.close()
-        #que_out.join_thread()
-
-        # Wait for producer to finish
-        producer.join()
-
-        for w in workers:
-            w.join()
-
         # Save data to pickle
         output_file = Path(args.output_dir) / f"trivial_ngrams_{args.split_name}.pkl"
         output_file.parent.mkdir(parents=True, exist_ok=True)
         output_file.write_bytes(pickle.dumps(data))
-        
 
-
-        # prevent adding anything more to the process pool and wait for all processes to finish
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-    """
-    python trivial_ngrams.py \
-        --dataset_name andstor/the_pile_github \
-        --dataset_config_name python \
-        --text_column_name text \
-        --tokenizer_name gpt2 \
-        --num_proc 10 \
-        --max_ngram 4 \
-        --num_top_ngrams 1000 \
-        --split_name train \
-        --output_dir tmp
-
-    """
-
-
-"""
---stream \
---revision refs/pr/8 \
-    """
